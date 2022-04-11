ff12c7f7e8d713490847cd1b0d014894321b4575
Francisco Adriano Alves <falves.upa@epl.gov.br>
2022-04-11 16:26:31 -0300

    Introduzindo php e app mobile

--- js/home.js
+++ js/home.js
@@ -4,14 +4,20 @@ document.querySelector('#form-busca').style.background = 'onfocus';
 return false;
 }
 };
+
 var banners = ["img/destaque-home.png", "img/destaque-home-2.png"];
+
 var bannerAtual = 0;
+
 function trocaBanner() {
 bannerAtual = (bannerAtual + 1) % 2;
 document.querySelector('.destaque img').src = banners[bannerAtual];
 }
+
 var timer = setInterval(trocaBanner, 4000);
+
 var controle = document.querySelector('.pause');
+
 controle.onclick = function() {
 if (controle.className == 'pause') {
 clearInterval(timer);
