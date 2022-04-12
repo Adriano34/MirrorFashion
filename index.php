7702ac0ff690a9b71b45796ae8309643370a6985
Francisco Adriano Alves <falves.upa@epl.gov.br>
2022-04-12 16:23:57 -0300

    formulando a pagina com css

--- index.php
+++ index.php
@@ -1,39 +1,4 @@
-<!DOCTYPE html>
-<html>
-
-<head>
-    
-    <title>Mirror Fashion</title>
-    <meta charset="utf-8">
-    <link rel="icon" type="image/x-icon" href="img/logo.png">
-    <link rel="stylesheet" href="css/reset.css">
-    <link rel="stylesheet" href="css/estilos.css">
-    <meta name="viewport" content="width=device-width">
-    <link rel="stylesheet" href="css/mobile.css" media="(max-width: 939px)">
-
-</head>
-
-<body>
-    <header class="container">       
-        <h1><img src="img/logo.png" alt="Mirror Fashion"></h1>
-        <p class="sacola">
-            Nenhum item na sacola de compras
-        </p>
-
-        <nav class="menu-opcoes">
-            <ul>
-                <li><a href="#">Sua Conta</a></li>
-                <li><a href="#">Lista de Desejos</a></li>
-                <li><a href="#">Cartão Fidelidade</a></li>
-                <li><a href="sobre.html">Sobre</a></li>
-                <li><a href="#">Ajuda</a></li>
-            </ul>
-        </nav>
-        <!--[if lt IE 9]>
-        <script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
-        <![endif]-->
-
-        <div class="container destaque">
+<div class="container destaque">
             <section class="busca">
                 <h2>Busca</h2>
                 <form action="http://www.google.com.br/search" id="form-busca">>
@@ -201,7 +166,7 @@
         </footer>
 
                        
-    </header>
+  
 
     <script src="js/home.js"></script>
     
