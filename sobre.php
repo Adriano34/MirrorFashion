26b1bde590080cf9b040fd3273b742823d1da893
Francisco Adriano Alves <falves.upa@epl.gov.br>
2022-04-20 15:15:14 -0300

    iframe

--- sobre.php
+++ sobre.php
@@ -3,3 +3,10 @@
 <title><?php print $cabecalho_title; ?> </title> 
 <?php include("cabecalho.php"); ?>
 <?php include("rodape.php"); ?>
+
+<iframe width="420" height="315" src="https://youtu.be/Tb06abHE4hY"
+frameborder="0" allowfullscreen></iframe>
+
+<iframe width="425" height="350" frameborder="0"
+scrolling="no" marginheight="0" marginwidth="0"
+src="http://maps.google.com.br/maps?q=Jacarezinho&amp;output=embed"></iframe>
